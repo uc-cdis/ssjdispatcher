30c02b5a858de55ee1adbffb024f2f1b304f6270
Giang Bui <giangbui0816@gmail.com>
2018-11-19 08:33:07 -0600

    feat(args): add args

--- main.go
+++ main.go
@@ -4,18 +4,21 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/uc-cdis/ssjdispatcher/ssjdispatcher"
 )
 
+// the path to aws credential
 const CREDPATH = "./credentials.json"
 
 // const (
 // 	QueueUrl = "https://sqs.us-east-1.amazonaws.com/440721843528/mySQS"
 // )
 
+// getSQSSession gets new sqs session
 func getSQSSession() (*sqs.SQS, error) {
 	awsClient := ssjdispatcher.AwsClient{}
 	awsClient.LoadCredentialFromConfigFile(CREDPATH)
@@ -24,6 +27,7 @@ func getSQSSession() (*sqs.SQS, error) {
 	return sqs.New(awsClient.GetClientSession()), nil
 }
 
+// getSQSMessages gets messages from queue
 func getSQSMessages(svc *sqs.SQS, queueUrl string) (*sqs.ReceiveMessageOutput, error) {
 	// Receive message
 	receiveParams := &sqs.ReceiveMessageInput{
@@ -51,11 +55,22 @@ func deleteSQSMessage(svc *sqs.SQS, queueUrl string, sqsMessage *sqs.Message) er
 
 }
 
+// go run main.go https://sqs.us-east-1.amazonaws.com/440721843528/mySQS '{"pattern1": "do_1", "pattern2": "do_2"}'
 func main() {
+
 	argsWithProg := os.Args
+	if len(argsWithProg) < 3 {
+		panic("The service requires 3. Only " + strconv.Itoa((len(argsWithProg))) + " provided")
+	}
+
 	queueURL := argsWithProg[1]
+	mappingStr := argsWithProg[2]
+
+	//mappingStr = "{\"pattern1\": \"do_1\", \"pattern2\": \"do_2\"}"
+	//queueURL = "https://sqs.us-east-1.amazonaws.com/440721843528/mySQS"
+
 	var mapping map[string]string
-	if err := json.Unmarshal([]byte(argsWithProg[2]), &mapping); err != nil {
+	if err := json.Unmarshal([]byte(mappingStr), &mapping); err != nil {
 		panic(err)
 	}
 
@@ -67,8 +82,7 @@ func main() {
 
 	svc, err := getSQSSession()
 	if err != nil {
-		log.Printf("Can not get SQS session. Detail %s", err)
-		panic("Can not get SQS session")
+		panic(err)
 	}
 	resp, _ := getSQSMessages(svc, queueURL)
 
@@ -77,9 +91,12 @@ func main() {
 		err := objectHandler.HandleS3Object(message)
 		if err != nil {
 			log.Println(err)
+		} else {
+			if deleteSQSMessage(svc, queueURL, message) != nil {
+			} else {
+				log.Printf("[Delete message] \nMessage ID: %s has beed deleted.\n\n", *message.MessageId)
+			}
 		}
-		deleteSQSMessage(svc, queueURL, message)
-
 	}
 
 }
