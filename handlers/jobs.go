2eeacdfec23e3e7c85a1a769d6393f0328384697
Giang Bui <giangbui0816@gmail.com>
2019-09-04 10:17:28 -0500

    feat(timeout): update timeout for job

--- handlers/jobs.go
+++ handlers/jobs.go
@@ -180,7 +180,7 @@ func CreateK8sJob(inputURL string, jobConfig JobConfig) (*JobInfo, error) {
 	randname := GetRandString(5)
 	name := fmt.Sprintf("%s-%s", jobConfig.Name, randname)
 	glog.Infoln("job input URL: ", inputURL)
-	var deadline int64 = 7200
+	var deadline int64 = 72000
 	labels := make(map[string]string)
 	labels["app"] = "ssjdispatcherjob"
 
