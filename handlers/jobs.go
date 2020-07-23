eb757186f8b05066e1e0a023e90925d36cf48aea
Giang Bui <giangbui0816@gmail.com>
2020-07-22 20:10:21 -0500

    feat(job): handle no aws credential provided

--- handlers/jobs.go
+++ handlers/jobs.go
@@ -181,6 +181,19 @@ func CreateK8sJob(inputURL string, jobConfig JobConfig) (*JobInfo, error) {
 	accessKeyIf, _ := GetValueFromJSON(credBytes, []string{"AWS", "aws_access_key_id"})
 	secretKeyIf, _ := GetValueFromJSON(credBytes, []string{"AWS", "aws_secret_access_key"})
 
+	awsRegion := ""
+	if regionIf != nil {
+		awsRegion = regionIf.(string)
+	}
+	awsAccessKey := ""
+	if accessKeyIf != nil {
+		awsAccessKey = accessKeyIf.(string)
+	}
+	awsSecretKey := ""
+	if secretKeyIf != nil {
+		awsSecretKey = secretKeyIf.(string)
+	}
+
 	configBytes, err := json.Marshal(jobConfig.ImageConfig)
 	if err != nil {
 		return nil, err
@@ -275,15 +288,15 @@ func CreateK8sJob(inputURL string, jobConfig JobConfig) (*JobInfo, error) {
 								},
 								{
 									Name:  "AWS_REGION",
-									Value: regionIf.(string),
+									Value: awsRegion,
 								},
 								{
 									Name:  "AWS_ACCESS_KEY_ID",
-									Value: accessKeyIf.(string),
+									Value: awsAccessKey,
 								},
 								{
 									Name:  "AWS_SECRET_ACCESS_KEY",
-									Value: secretKeyIf.(string),
+									Value: awsSecretKey,
 								},
 								{
 									Name:  "CONFIG_FILE",
